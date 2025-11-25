7ebf7f3fb7c0277f40e7a51dd109e1214482e853
vishvajeet-y <vishvajeetyadav@VISHVAJEETs-Laptop.local>
2025-11-26 02:21:41 +0530

    Added Prod & QA profile along with diffrent way to access property value

--- card/src/main/java/com/vish/card/dto/CardContactInfoDto.java
+++ card/src/main/java/com/vish/card/dto/CardContactInfoDto.java
@@ -1,4 +1,10 @@
 package com.vish.card.dto;
 
-public record CardContactInfoDto() {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cards")
+public record CardContactInfoDto(String message, Map<String,String>contactDetails, List<String> onCallSupport) {
 }
