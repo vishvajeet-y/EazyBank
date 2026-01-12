3f4af9b5136ab3b4a988c5ed21e2e9027e75f23f
vishvajeet-y <vishvajeetyadav@VISHVAJEETs-Laptop.local>
2026-01-13 00:14:03 +0530

    Integrating Spring Cloud Bus & RabbitMQ  to fetch data from configserver

--- card/src/main/java/com/vish/card/dto/CardContactInfoDto.java
+++ card/src/main/java/com/vish/card/dto/CardContactInfoDto.java
@@ -1,10 +1,26 @@
 package com.vish.card.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
-
+@ConfigurationProperties(prefix = "cards")
+@Getter
+@Setter
+public class CardContactInfoDto {
+    private  String message;
+    private Map<String,String>contactDetails;
+    private  List<String>onCallSupport;
+}
+/*
+Previously Record class was holding properties value and we cannot change value of these properties
+by invoking setter method as all our field are going to be final.. Once value of our field are going to
+be created using constructor there is no way to change value inside these fields..
+ */
+/*
 @ConfigurationProperties(prefix = "cards")
 public record CardContactInfoDto(String message, Map<String,String>contactDetails, List<String> onCallSupport) {
 }
+*/
